472094a472c17ba791b1ef10c0cfca00311a62dc
stinkycats <772741048@qq.com>
2020-10-11 12:16:43 +0800

    update

---
+++ Array/CN/16.最接近的三数之和.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=16 lang=cpp
+ *
+ * [16] 最接近的三数之和
+ */
+
+// @lc code=start
+class Solution 
+{
+public:
+    int threeSumClosest(vector<int>& nums, int target) 
+    {
+        //ergotic i,j,k
+        int closestval = 0;
+        int minval = INT_MAX;
+        for(int i = 0; i<nums.size(); i++)
+            for(int j = i + 1; j<nums.size(); j++)
+                for(int k = j + 1; k<nums.size(); k++)
+                    //judge condition
+                    if(abs((nums[i] + nums[j] + nums[k]) - target) <= minval)
+                    {
+                        minval = abs((nums[i] + nums[j] + nums[k]) - target);
+                        closestval = (nums[i] + nums[j] + nums[k]);
+                    }
+        return closestval;
+    }
+};
+// @lc code=end
+
